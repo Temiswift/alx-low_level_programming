c2537a688fd163b6949f096de569413e1647611a
odebe <odebedaniel@gmail.com>
2022-09-09 02:46:43 -0700

    fixed 5,6,101

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>>
+
+/**
+ * main - print string
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art\n");
+return (0);
+}
